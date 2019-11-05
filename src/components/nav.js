b91e7972c58a63e2d5d480b28713a6566d3ac098
rabbyrevalde <rabby.revalde@webriq.services>
2019-11-05 14:44:37 +0800

    partial update

---
+++ src/components/nav.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import { Button, Navbar, Nav, Form, FormControl } from "react-bootstrap";
+
+const navbar = props => {
+  //   const [isOpen, setIsOpen] = useState(false);
+
+  //   const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar bg="primary" variant="dark">
+        <Navbar.Brand href="#home">WMS</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
+        <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <Button variant="outline-light">Search</Button>
+        </Form>
+      </Navbar>
+    </div>
+  );
+};
+
+export default navbar;
